c99a1dd92cf9ba0dd4cd0769a9d88d79e5b1cf54
Tristan Dyer <atdyer@ncsu.edu>
2019-10-30 21:41:11 -0400

    Brainstorming

--- test/play.js
+++ test/play.js
@@ -14,20 +14,75 @@ function setInstance (instance) {
 
     let { sets, relations } = simplify(instance);
 
-    let matrix = 'Matrix$0';
-    let vals = relations['this/Matrix<:vals'];
-    let matrixvals = join(matrix, vals);
+    /**
+    This pattern could lend itself well to something similar to D3
+    selections, see here https://bost.ocks.org/mike/selection/
 
-    const parseCell = (tuple) => {
-        const row = parseInt(tuple[0]);
-        const col = parseInt(tuple[1]);
-        const val = tuple[2];
-        return { row, col, val };
-    };
+    Our "selections" would be arrays of arrays as well. Signatures are
+    represented as sets: arrays of arrays in which each inner array has
+    only a single item (an atom).
 
-    let cells = matrixvals.map(parseCell);
-    console.log(cells);
+        matrix = [['Matrix$0'], ['Matrix$1']]
 
+    Fields are represented as relations: arrays of arrays in which each inner
+    array can have multiple items (also atoms).
+
+        vals = [
+            ['Matrix$0', '0', '0', 'Value$0'],
+            ['Matrix$0', '0', '1', 'Value$0'],
+            ...
+        ]
+
+    And then we have an API built around these "selection" so that we can do
+    things like:
+
+        matrix.property('vals', vals, accessor);
+
+    Wherein we do something like below to match rows of the vals relations with
+    each atom in the matrix set.
+
+     */
+
+    let matrices = data(sets['this/Matrix']);
+    let rows = relations['this/Matrix<:rows'];
+    property(matrices, 'rows', rows, matches => parseInt(matches[0][0]));
+    console.log(matrices);
+
+    // let matrix = 'Matrix$0';
+    // let vals = relations['this/Matrix<:vals'];
+    // let matrixvals = join(matrix, vals);
+    //
+    // const parseCell = (tuple) => {
+    //     const row = parseInt(tuple[0]);
+    //     const col = parseInt(tuple[1]);
+    //     const val = tuple[2];
+    //     return { row, col, val };
+    // };
+    //
+    // let cells = matrixvals.map(parseCell);
+    // console.log(cells);
+
+}
+
+// Turn a list of atoms into a list of objects. These objects will then be
+// operated upon by subsequent functions
+function data (set) {
+    return set.map(item => ({ datum: item }));
+}
+
+function property (data, name, tuples, accessor) {
+    data.forEach(item => {
+        const datum = item.datum;
+        const matches = match(datum, tuples);
+        item[name] = accessor(matches);
+    });
+    return data;
+}
+
+function match (atom, tuples) {
+    return tuples
+        .filter(tuple => tuple.includes(atom))
+        .map(tuple => tuple.filter(tupat => tupat !== atom))
 }
 
 function join (atom, tuples) {
